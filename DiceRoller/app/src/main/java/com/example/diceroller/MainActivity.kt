49de2debff4d3536943e9596e32772e49597ae9a
xieeryihe <2640182777@qq.com>
2023-02-02 13:05:27 +0800

    DiceRollerFinal-challenge

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,23 +6,28 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var diceImage : ImageView  // added
+    private lateinit var diceImage1 : ImageView
+    private lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_image)  // added
+        diceImage1 = findViewById(R.id.dice_image_1)
+        diceImage2 = findViewById(R.id.dice_image_2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
     private fun rollDice() {
-        val drawableResource = when ((1..6).random()) {
-            1 -> R.drawable.dice_2
+        diceImage1.setImageResource( getRandomDiceImage() )
+        diceImage2.setImageResource( getRandomDiceImage() )
+    }
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()) {
+            1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
     }
 }
