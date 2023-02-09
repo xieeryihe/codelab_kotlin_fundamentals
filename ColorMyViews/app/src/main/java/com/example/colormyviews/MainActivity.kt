f85d3a250603a7606430eb7f2ee155a63200b60c
xieeryihe <2640182777@qq.com>
2023-02-09 20:46:42 +0800

    lab7: stage2 basic finished. (after coding challenge1, before coding challenge2)

--- ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +22,18 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+            /*
+            上面三行有点繁琐，因为setBackgroundResource必须要对对象引用，不能是id，样例代码这块直接给的是：
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            显然这块 box_three_text 在本函数中都没有引用（即使有也是id，并非对象）。
+            github上的代码也没有声明类成员，我感觉这块写错了
+            已经给样例代码写feedback了，不知道他们能不能看到。
+             */
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -31,10 +44,16 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(item) }
